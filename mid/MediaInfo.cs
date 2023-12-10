bd00b46e62fe10fa51becea6ee27bf29512bebbf
Hannosset <c.hannosset@yandex.com>
2023-12-10 13:42:41 +0800

    mid V1 - OOM

---
+++ mid/MediaInfo.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+using VideoLibrary;
+
+namespace mid
+{
+	internal class MediaInfo
+	{
+		#region TYPES
+		internal class InfoData
+		{
+			#region TYPES
+			internal enum MediaType { Other, Audio, Video };
+			internal enum MediaFormat { Other, acc, vorbis, opus, mp4, webm };
+			internal enum AudioFormat { Any, acc, vorbis, opus };
+			#endregion
+
+			#region ACCESSORS
+			internal MediaType Type { get; private set; } = MediaType.Other;
+			internal MediaFormat Format { get; private set; } = MediaFormat.Other;
+			internal decimal Resolution { get; private set; }
+			internal AudioFormat VideoAudio { get; private set; } = AudioFormat.Any;
+			internal long AudioBitrate { get; private set; } = -1;
+			internal string Uri { get; private set; }
+			#endregion
+
+			#region CONSTRUCTOR
+			internal InfoData( YouTubeVideo webvideo )
+			{
+				Uri = webvideo.Uri;
+				if( webvideo.AdaptiveKind == AdaptiveKind.Audio )
+				{
+					Type = MediaType.Audio;
+					switch( webvideo.AudioFormat )
+					{
+						case VideoLibrary.AudioFormat.Aac: VideoAudio = AudioFormat.acc; break;
+						case VideoLibrary.AudioFormat.Vorbis: VideoAudio = AudioFormat.vorbis; break;
+						case VideoLibrary.AudioFormat.Opus: VideoAudio = AudioFormat.opus; break;
+					}
+					AudioBitrate = webvideo.AudioBitrate;
+				}
+				else
+				{
+					Type = MediaType.Video;
+					switch( webvideo.Format )
+					{
+						default:
+						case VideoFormat.Mp4: Format = MediaFormat.mp4; break;
+						case VideoFormat.WebM: Format = MediaFormat.webm; break;
+					}
+					Resolution = webvideo.Resolution;
+					switch( webvideo.AudioFormat )
+					{
+						case VideoLibrary.AudioFormat.Aac: VideoAudio = AudioFormat.acc; break;
+						case VideoLibrary.AudioFormat.Vorbis: VideoAudio = AudioFormat.vorbis; break;
+						case VideoLibrary.AudioFormat.Opus: VideoAudio = AudioFormat.opus; break;
+					}
+					AudioBitrate = webvideo.AudioBitrate;
+				}
+			}
+			#endregion
+
+			#region PUBLIC METHODS
+			public override string ToString()
+			{
+				if( Type == MediaType.Audio )
+					return $"{Type}\t{VideoAudio}\t{AudioBitrate}\t{Uri}";
+				else
+					return $"{Type}\t{Format}\t{Resolution}\t{VideoAudio}\t{AudioBitrate}\t{Uri}";
+			}
+			#endregion
+		}
+		#endregion
+
+		#region LOCAL VARIABLES
+		List<InfoData> _infoDatas = new List<InfoData>();
+		#endregion
+
+		#region ACCESSORS
+		internal string Title { get; private set; }
+		internal string Author { get; private set; }
+		internal string Url { get; private set; }
+		#endregion
+
+		#region CONSTRUCTOR
+		public MediaInfo( string url )
+		{
+			Url = url;
+			IEnumerable<YouTubeVideo> videos = YouTube.Default.GetAllVideos( url );
+
+			if( videos.Count() > 0 )
+			{
+				Title = videos.First().Title;
+				Author = videos.First().Info.Author;
+				foreach( YouTubeVideo item in videos )
+					_infoDatas.Add( new InfoData( item ) );
+			}
+		}
+		#endregion
+
+		#region PUBLIC METHODS
+		public override string ToString() => $"\"{Title}\"\t\"{Author}\"\t{_infoDatas.Count()}\n" + string.Join( "\n" , _infoDatas );
+		#endregion
+	}
+}
--- mid/Program.cs
+++ mid/Program.cs
@@ -1,15 +1,144 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Net;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
+
+using VideoLibrary;
+using System.Web;
 
 namespace mid
 {
 	internal class Program
 	{
+		static string ReadStream( Stream strm )
+		{
+			StringBuilder sb = new StringBuilder();
+			{
+				byte[] buffer = new byte[16 * 1024];
+				int bytesRead = 0;
+				do
+				{
+					bytesRead = strm.Read( buffer , 0 , buffer.Length );
+					if( bytesRead > 0 )
+						sb.Append( Encoding.Default.GetString( buffer , 0 , bytesRead ) );
+				} while( bytesRead > 0 );
+			}
+			return sb.ToString();
+		}
+		static string[] ExtractListFromHtml( string sb )
+		{
+			List<string> lst = new List<string>();
+
+			if( sb.Length > 0 )
+			{
+				string http = HttpUtility.HtmlDecode( sb.ToString() );
+				for( int i = 1 ; i < 200 ; i++ )
+				{
+					string tag = $"index={i}";
+					int at = http.IndexOf( tag );
+					if( at > 0 )
+					{
+						int from = http.LastIndexOf( "/watch?v=" , at );
+						lst.Add( http.Substring( from , at - from + tag.Length ) );
+					}
+				}
+			}
+			return lst.ToArray();
+		}
+		static string[] DownloadPlaylist( string uri )
+		{
+
+			HttpWebRequest w_request = null;
+			WebResponse w_response = null;
+			w_request.ServicePoint.MaxIdleTime = 1000;
+			w_request.KeepAlive = false;
+			w_request.Timeout = 300000;
+
+			for( int attempt = 0 ; attempt < 10 ; attempt++ )
+				try
+				{
+					w_request = (HttpWebRequest)WebRequest.Create( uri );
+					if( w_request != null )
+					{
+						w_response = w_request.GetResponse();
+						if( w_response != null )
+						{
+							using( Stream streamweb = w_response.GetResponseStream() )
+								return ExtractListFromHtml( ReadStream( streamweb ) );
+						}
+						break;
+					}
+				}
+				catch( WebException ex )
+				{
+					if( ex.Status == WebExceptionStatus.ProtocolError )
+					{
+						Console.Error.WriteLine( "Error --> skip task and restart after several hours" );
+						break;
+					}
+					if( ex.Status == WebExceptionStatus.Timeout )
+					{
+						Console.Error.WriteLine( "WARNING -- > wait a few secs" );
+						System.Threading.Thread.Sleep( 2000 );
+					}
+					else if( ex.Status == WebExceptionStatus.Pending )
+					{
+						w_request.Timeout += 10000;
+						Console.Error.WriteLine( $"WARNING -- > Increase timeout to {w_request.Timeout} sec" );
+					}
+					else
+						Console.Error.WriteLine( "EXCEPTION: " + ex.Message );
+				}
+				catch( IOException ex )
+				{
+					if( ex.HResult == -2146232800 )  //	Received an unexpected EOF or 0 bytes from the transport stream.
+					{
+						Console.Error.WriteLine( " -- > wait a few secs" );
+						System.Threading.Thread.Sleep( 2000 );
+					}
+					else
+					{
+						Console.Error.WriteLine( "IOEXCEPTION: " + ex.Message );
+						break;
+					}
+				}
+				catch( Exception ex )
+				{
+					Console.Error.WriteLine( $"{ex.GetType().FullName}: {ex.Message}" );
+					break;
+				}
+				finally
+				{
+					w_response?.Close();
+					w_response?.Dispose();
+					w_response = null;
+
+					w_request?.Abort();
+					w_request = null;
+
+					GC.Collect();
+					GC.WaitForPendingFinalizers();
+					GC.Collect();
+				}
+			return Array.Empty<string>();
+		}
+
 		static void Main( string[] args )
 		{
+			List<MediaInfo> VideoList = new List<MediaInfo>();
+
+			foreach( string url in args )
+				if( url.ToLower().IndexOf( "http" ) == 0 )
+					if( url.Contains( "&list=" ) )
+						foreach( string url_ in DownloadPlaylist( url ) )
+							Console.Out.Write( new MediaInfo( url_ ) );
+					else
+						Console.Out.Write(	new MediaInfo( url ) );
 		}
 	}
 }
